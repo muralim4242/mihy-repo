e840ca2b59a319affb2552e10af0396cf4cf4c42
sudhanshu <sudhanshu.s.deshmukh@gmail.com>
2018-09-10 15:40:53 +0530

    added owner info tab

--- packages/egov-tradelicence/src/ui-config/screens/specs/tradelicence/apply.js
+++ packages/egov-tradelicence/src/ui-config/screens/specs/tradelicence/apply.js
@@ -151,12 +151,50 @@ const tradeLocationDetails = getCommonCard({
   })
 });
 
+const OwnerInfoCard = getCommonGrayCard({
+  header: getCommonSubHeader("Owner Information"),
+  break: getBreak(),
+  tradeUnitCardContainer: getCommonContainer({
+    ownerMobileNo: getTextField("Mobile No.", "Enter Mobile No.", true, ""),
+    ownerName: getTextField("Name", "Enter Name", true, ""),
+    ownerFatherName: getTextField(
+      "Father/Husband's Name",
+      "Enter Father/Husband's Name",
+      true,
+      ""
+    ),
+    OwnerGender: getSelectTextField("Gender", "Select Gender", true, ""),
+    ownerDOB: getTextField("Date of Birth", "Enter Date of Birth", true, ""),
+    ownerEmail: getTextField("Email", "Enter Email", false, ""),
+    ownerPAN: getTextField("PAN No.", "Enter Owner's PAN No.", false, ""),
+    ownerAddress: getTextField(
+      "Corrospondence Address",
+      "Enter Corrospondence Address",
+      true,
+      ""
+    ),
+    OwnerSpecialCategory: getSelectTextField(
+      "Special Owner Category",
+      "Select Special Owner Category",
+      true,
+      ""
+    )
+  })
+});
+
 const tradeOwnerDetails = getCommonCard({
   header: getCommonSubHeader("Please Provide Trade Owner Details"),
   break: getBreak(),
   paragraph: getCommonParagraph(
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard Lorem Ipsum has been the industry's standard."
-  )
+  ),
+  tradeLicenseType: getSelectTextField(
+    "Type of ownership",
+    "Select Type of Ownership",
+    false,
+    ""
+  ),
+  OwnerInfoCard
 });
 const tradeDocumentDetails = getCommonCard({
   header: getCommonSubHeader(
@@ -201,8 +239,8 @@ const screenConfig = {
           componentPath: "Div",
           children: {
             tradeOwnerDetails
-          },
-          visible: false
+          }
+          // visible: false
         },
         formwizardThreeStep: {
           uiFramework: "custom-atoms",
