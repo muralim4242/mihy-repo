0f38a00ca5e6c7184d04e451a954735df4ada616
sudhanshu <sudhanshu.s.deshmukh@gmail.com>
2018-10-01 14:04:20 +0530

    updated date regex

--- packages/mihy-ui-framework-core/src/ui-config/screens/specs/utils/index.js
+++ packages/mihy-ui-framework-core/src/ui-config/screens/specs/utils/index.js
@@ -215,7 +215,7 @@ export const getDateField = (
       required,
       iconObj,
       jsonPath,
-      type:"date"
+      type: "date"
     },
     gridDefination,
     required,
@@ -470,7 +470,7 @@ export const getPattern = type => {
     case "TradeName":
       return /^[a-zA-Z0-9\s()-@#&.,?/]{1,100}$/i;
     case "Date":
-      return /(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13578]|1[02]))|((29|30)[/](0[4,6,9]|11)))[/](19|[2-9][0-9])dd$)|(^29[/]02[/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/i;
+      return /^[12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/i;
     case "UOMValue":
       return /^[1-9][0-9]{0,3}$/i;
     case "OperationalArea":
