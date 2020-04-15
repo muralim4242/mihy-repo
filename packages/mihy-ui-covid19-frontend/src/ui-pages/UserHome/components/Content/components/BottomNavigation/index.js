ef87aadda3aa27414446f8c31dccbe0b39b8577d
muralimgwl <murali.m@goodworklabs.com>
2020-04-16 03:06:33 +0530

    bottom navivation change for comming soon option

--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/components/BottomNavigation/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/components/BottomNavigation/index.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import Icon from "@material-ui/core/Icon";
-
 
 const styles = {
   root: {
     width: "100%",
-    position:"fixed",
-    bottom:"0px",
-    zIndex:"1000"
-  },
+    position: "fixed",
+    bottom: "0px",
+    zIndex: "1000"
+  }
 };
 
 class SimpleBottomNavigation extends React.Component {
   state = {
-    value: 0,
+    value: 0
   };
 
   handleChange = (event, value) => {
+    const { t, setAppData } = this.props;
     this.setState({ value });
+    if (value) {
+      setAppData("snackbar", {
+        open: true,
+        variant: "success",
+        message: t("Comming Soon!")
+      });
+    }
   };
 
   render() {
-    const { classes ,menuItems=[],t} = this.props;
+    const { classes, menuItems = [], t } = this.props;
     const { value } = this.state;
 
     return (
@@ -35,18 +42,22 @@ class SimpleBottomNavigation extends React.Component {
         showLabels
         className={classes.root}
       >
-      {menuItems.map((menu, key) => {
-        return (
-          <BottomNavigationAction key={key} label={t(menu.name)} icon={<Icon color="primary">{menu.icon}</Icon>} />
-        );
-      })}
+        {menuItems.map((menu, key) => {
+          return (
+            <BottomNavigationAction
+              key={key}
+              label={t(menu.name)}
+              icon={<Icon color="primary">{menu.icon}</Icon>}
+            />
+          );
+        })}
       </BottomNavigation>
     );
   }
 }
 
 SimpleBottomNavigation.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(SimpleBottomNavigation);
