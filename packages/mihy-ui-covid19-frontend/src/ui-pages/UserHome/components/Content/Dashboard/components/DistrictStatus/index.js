0fb4ab2bf376796582b6dc5f95a77feca966ac1d
muralimgwl <murali.m@goodworklabs.com>
2020-04-15 17:09:07 +0530

    added color to confirmed case

--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/Dashboard/components/DistrictStatus/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/Dashboard/components/DistrictStatus/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
-const DistrictStatus = ({ status = {}, t }) => {
+const DistrictStatus = ({ status = {},selectedState, t }) => {
   return (
     <div>
       <Typography variant="h6" gutterBottom color="primary">
-        {t(status.code)}
+        {t(`district.${selectedState}.${status.code}`)}
       </Typography>
-      <Typography variant="subtitle2" gutterBottom align="center">
+      <Typography variant="subtitle2" gutterBottom align="center" style={{color:"#f44336"}}>
         {`${t("confirmed")} - ${status.confirmed} ${parseInt(status.delta)?`[+${status.delta}]`:''}`}
       </Typography>
     </div>
