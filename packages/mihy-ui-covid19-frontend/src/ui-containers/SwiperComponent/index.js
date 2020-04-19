b17ff0689ac76ec124e87c6caec1b7e3d928dfad
Poojadatyal <pooja.datyal19@gmail.com>
2020-04-19 18:27:32 +0530

    Added each profile content to the swiper

--- packages/mihy-ui-covid19-frontend/src/ui-containers/SwiperComponent/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-containers/SwiperComponent/index.js
@@ -1,22 +1,156 @@
 import React from "react";
-import { withStyles, Avatar } from '@material-ui/core';
+import { withStyles, Avatar, Grid, Typography } from '@material-ui/core';
 import Swiper from "swiper";
 import "swiper/css/swiper.css";
 import "./index.css";
+import classNames from 'classnames';
 
-const styles = {
+const styles = theme => ({
   bigAvatar: {
     margin: 10,
     width: 260,
-    height: 260
+    height: 260,
+  },
+  nameStyle: {
+    color: "#d81b60",
+    fontSize: 28,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 24
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 24
+    },
+    fontWeight: 600,
+    margin: "10px 0px",
+    textAlign: "center"
+  },
+  roleStyle: {
+    fontSize: 22,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 18
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 18
+    },
+    fontWeight: 400,
+    color: "grey",
+    marginBottom: 10,
+    textAlign: "center"
+  },
+  heading: {
+    fontSize: 16,
+    fontWeight: 600,
+  },
+  // mainContainer: {
+  //   position: "absolute",
+  //   top: "70%",
+  //   left: "50%",
+  //   marginLeft: "-50%",
+  //   marginTop: "0%"
+  // },
+  contentContainer: {
+    margin: "20px 0px"
+  },
+  data: {
+    fontSize: 16,
+    color: "grey"
+  },
+  grid: {
+    margin: "10px 0px",
+    [theme.breakpoints.down("xs")]: {
+      margin: "3px 0px"
+    },
+    display: "flex",
+    justifyContent: "center"
+  },
+  placeGrid: {
+    margin: "10px 0px 75px 0px",
+    textAlign: "right",
+    padding: "0px 40px",
+    fontSize: 16,
+    [theme.breakpoints.down("sm")]: {
+      fontSize: 14
+    },
+    [theme.breakpoints.down("xs")]: {
+      fontSize: 14
+    },
+    color: "grey"
+  },
+  swiperContainer: {
+    [theme.breakpoints.down("sm")]: {
+      top: "35%"
+    },
+    [theme.breakpoints.down("xs")]: {
+      top: "32%"
+    }
   }
-}
-
+});
+const data = [
+  {
+    name: "Murali. M",
+    role: "Fullstack Developer",
+    roleInCovid19: "Fullstack Developer",
+    skills: "JS, React JS, Node JS and DevOPS",
+    qualification: "B.E. in ISE, Diploma in CSE",
+    from: "Shimoga, Karnataka",
+    src: "assets/murali.jpeg"
+  },
+  {
+    name: "Thahareema",
+    role: "UI/UX Designer",
+    roleInCovid19: "UI/UX Designer",
+    skills: "Adobe XD and Product Management",
+    qualification: "Diploma in ECE",
+    from: "Chaamaraajanagara, Karnataka",
+    src: "assets/thahareema.jpeg"
+  },
+  {
+    name: "Praveen M P",
+    role: "Fullstack Developer",
+    roleInCovid19: "Frontend Developer",
+    skills: "Javascript, React, Angular",
+    qualification: "MCA",
+    from: "Bangalore",
+    src: "assets/praveen.jpeg"
+  },
+  {
+    name: "Shalu Singh",
+    role: "Fullstack Developer",
+    roleInCovid19: "Frontend Developer",
+    skills: "JS, React, Redux, Material-ui",
+    qualification: "M.Tech in CSE, VIT",
+    from: "Bulandshahar, Uttar Pradesh",
+    src: "assets/shalu.jpeg"
+  },
+  {
+    name: "Bikki Mahato",
+    role: "Fullstack Developer",
+    roleInCovid19: "Frontend Developer",
+    skills: "JS, React, Redux, Material-ui, Node JS",
+    qualification: "B.Tech in IT",
+    from: "Kolkata, West Bengal",
+    src: "assets/bikki.jpeg"
+  },
+  {
+    name: "Pooja Datyal",
+    role: "Fullstack Developer",
+    roleInCovid19: "Frontend Developer",
+    skills: "JS, React, Redux, Material-ui",
+    qualification: "B.Tech in CSE",
+    from: "Zirakpur, Punjab",
+    src: "assets/girl.jpg"
+  },
+  {
+    name: "Gagan",
+    role: "Fullstack Developer",
+    roleInCovid19: "Frontend Developer",
+    src: "assets/boy.jpg"
+  }
+]
 class SwiperComponent extends React.Component {
 
   componentDidMount() {
-    const { setAppData } = this.props;
-    var swiper = new Swiper('.swiper-container', {
+    new Swiper('.swiper-container', {
       effect: 'cube',
       grabCursor: true,
       cubeEffect: {
@@ -28,76 +162,9 @@ class SwiperComponent extends React.Component {
       pagination: {
         el: '.swiper-pagination',
       },
-       autoplay: {
+      autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-      }
-    });
-    swiper.on('transitionEnd', function () {
-      console.log('*** mySwiper.realIndex', swiper.activeIndex);
-      switch (swiper.activeIndex) {
-        case 0:
-          setAppData('aboutUsContent', {
-            name: "Murali. M",
-            role: "Fullstack Developer",
-            skills: "JS, React JS, Node JS and DevOPS",
-            qualification: "B.E. in ISE,\nDiploma in CSE",
-            from: "Shimoga, Karnataka"
-          });
-          break;
-        case 1:
-          setAppData('aboutUsContent', {
-            name: "Thahareema",
-            role: "UI/UX Designer",
-            skills: "Adobe XD and Product Management",
-            qualification: "Diploma in ECE",
-            from: "Chaamaraajanagara, Karnataka"
-          }
-          )
-          break;
-        case 2:
-          setAppData('aboutUsContent', {
-            name: "Shalu Singh",
-            role: "Frontend Developer",
-            skills: "JS, React, Redux, Material-ui",
-            qualification: "M.Tech in CSE, VIT",
-            from: "Bulandshahar, Uttar Pradesh"
-          }
-          )
-          break;
-        case 3:
-          setAppData('aboutUsContent', {
-            name: "Bikki Mahato",
-            role: "Frontend Developer",
-            skills: "JS, React, Redux, Material-ui, Node JS",
-            qualification: "B.Tech in IT",
-            from: "Kolkata, West Bengal"
-          }
-          )
-          break;
-        // case 4:
-        // setAppData('aboutUsContent', {
-        //   name: "Pooja Datyal",
-        //   role: "Frontend Developer",
-        //   skills: "JS, React, Redux, Material-ui",
-        //   qualification: "B.Tech in CSE",
-        //   from: "Zirakpur, Punjab"
-        // }
-        // )
-        // break;
-        // case 5:
-        // setAppData('aboutUsContent', {
-        //   name: "Thahareema",
-        //   role: "UI/UX Designer",
-        //   skills: "Adobe XD and Product Management",
-        //   qualification: "Diploma in ECE",
-        //   from: "Chaamaraajanagara, Karnataka"
-        // }
-        // )
-        // break;
-        default:
-          setAppData('aboutUsContent', {});
-          break;
       }
     });
   };
@@ -105,34 +172,49 @@ class SwiperComponent extends React.Component {
   render() {
     const { classes } = this.props;
     return (
-      <div class="swiper-container">
+      <div className={classNames(classes.swiperContainer, 'swiper-container')}>
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <Avatar alt="Murali. M" src="assets/murali.jpeg" className={classes.bigAvatar} />
-          </div>
-          <div class="swiper-slide">
-            <Avatar alt="Thahareema" src="assets/thahareema.jpeg" className={classes.bigAvatar} />
-          </div>
-          <div class="swiper-slide">
-            <Avatar alt="Shalu Jadaun" src="assets/shalu.jpeg" className={classes.bigAvatar} />
-          </div>
-          <div class="swiper-slide">
-            <Avatar alt="Bikki Mahato" src="assets/bikki.jpeg" className={classes.bigAvatar} />
-          </div>
-          {/* <div class="swiper-slide">
-          <Avatar alt="Pooja Datyal" src="assets/pooja.jpeg" className={classes.bigAvatar} />
-            <img src="assets/pooja.jpg" alt="Pooja Datyal" width="100px" />
-          </div>
-          <div class="swiper-slide">
-            <img src="assets/suga.jpg" alt="suga" width="100px" />
-          </div>
-          <div class="swiper-slide">
-            <img src="assets/jhope.jpg" alt="jhope" width="100px" />
-          </div> */}
+          {data.map(d => {
+            return (
+              <div class="swiper-slide">
+                <Avatar alt={d.name} src={d.src} className={classes.bigAvatar} />
+                <Grid item md={12} xs={12} sm={12} className={classes.mainContainer}>
+                  <Grid container>
+                    <Grid item md={12} sm={12} xs={12}>
+                      <Grid container className={classes.contentContainer}>
+                        <Grid item md={12} sm={12} xs={12} className={classes.nameStyle}><em>{d.name}</em></Grid>
+                        <Grid item md={12} sm={12} xs={12} className={classes.roleStyle}>
+                          <em>{d.role}</em>
+                        </Grid>
+                        <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                          <Typography className={classes.heading}><em>Role in Covid 19:&nbsp;</em></Typography>
+                          <Typography className={classes.data}><em>{d.roleInCovid19}</em></Typography>
+                        </Grid>
+                        {d.skills && <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                          <Typography className={classes.heading}><em>Skills:&nbsp;</em></Typography>
+                          <Typography className={classes.data}><em>{d.skills}</em></Typography>
+                        </Grid>}
+                        {d.qualification && <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                          <Typography className={classes.heading}><em>Qualification:&nbsp;</em></Typography>
+                          <Typography className={classes.data}><em>{d.qualification}</em></Typography>
+                        </Grid>}
+                        {d.from && <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                          <Typography className={classes.heading}><em>From:&nbsp;</em></Typography>
+                          <Typography className={classes.data}><em>{d.from}</em></Typography>
+                        </Grid>}
+                      </Grid>
+                    </Grid>
+                    {/* <Grid item md={12} sm={12} xs={12} className={classes.placeGrid}>
+                      <em>{d.from}</em>
+                    </Grid> */}
+                  </Grid>
+                </Grid>
+              </div>
+            )
+          })}
         </div>
         <div class="swiper-pagination"></div>
       </div>
-
     );
   }
 };
--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/components/AboutUs/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/components/AboutUs/index.js
@@ -1,147 +1,18 @@
 import React from "react";
-import { withStyles, Typography, Grid, Hidden } from '@material-ui/core';
+import { Grid} from '@material-ui/core';
 import Swiper from "../../../../../../ui-containers/SwiperComponent";
-import isEmpty from "lodash/isEmpty";
-import { mapDispatchToProps } from "../../../../../../ui-utils/commons";
-import { connect } from "react-redux";
 
-const styles = theme => ({
-  nameStyle: {
-    color: "#d81b60",
-    fontFamily: "Bree Serif",
-    fontSize: 30,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 24
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: 24
-    },
-    fontWeight: 600,
-    margin: "10px 0px",
-    textAlign: "center"
-  },
-  roleStyle: {
-    fontSize: 24,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 18
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: 18
-    },
-    fontWeight: 400,
-    color: "grey",
-    marginBottom: 10,
-    textAlign: "center"
-  },
-  heading: {
-    fontSize: 22,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 16
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: 16
-    },
-    fontWeight: 600,
-  },
-  mainContainer: {
-    position: "absolute",
-    top: "70%",
-    left: "50%",
-    marginLeft: "-50%",
-    marginTop: "0%"
-  },
-  contentContainer: {
-    margin: "25px 0px"
-  },
-  data: {
-    fontSize: 22,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 16
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: 16
-    },
-    color: "grey"
-  },
-  grid: {
-    margin: "10px 0px",
-    display: "flex",
-    justifyContent: "center"
-  },
-  placeGrid: {
-    margin: "10px 0px 75px 0px",
-    textAlign: "right",
-    padding: "0px 40px",
-    fontSize: 20,
-    [theme.breakpoints.down("sm")]: {
-      fontSize: 14
-    },
-    [theme.breakpoints.down("xs")]: {
-      fontSize: 14
-    },
-    color: "grey"
-  }
-
-})
 class AboutUs extends React.Component {
 
   render() {
-    const { classes, aboutUsContent, setAppData } = this.props;
-    const { name, role, skills, qualification, from } = aboutUsContent;
-
     return (
       <Grid container>
         <Grid item md={12} sm={12} xs={12}>
-          <Swiper setAppData={setAppData} />
+          <Swiper />
         </Grid>
-        {!isEmpty(aboutUsContent) &&
-          <Grid item md={12} xs={12} sm={12} className={classes.mainContainer}>
-            <Grid container>
-              <Hidden mdDown><Grid item md={3}></Grid></Hidden>
-              <Grid item md={6} sm={12} xs={12}>
-                <Grid container className={classes.contentContainer}>
-                  <Grid item md={12} sm={12} xs={12} className={classes.nameStyle}><em>{name}</em></Grid>
-                  <Grid item md={12} sm={12} xs={12} className={classes.roleStyle}>
-                    <em>{role}</em>
-                  </Grid>
-                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
-                    <Typography className={classes.heading}><em>Skills:&nbsp;</em></Typography>
-                    <Typography className={classes.data}><em>{skills}</em></Typography>
-                  </Grid>
-                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
-                    <Typography className={classes.heading}><em>Qualification:&nbsp;</em></Typography>
-                    <Typography className={classes.data}><em>{qualification}</em></Typography>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Hidden mdDown><Grid item md={3} sm={3} xs={3}></Grid></Hidden>
-              <Grid item md={12} sm={12} xs={12} className={classes.placeGrid}>
-                <em>{from}</em>
-              </Grid>
-            </Grid>
-          </Grid>
-        }
       </Grid>
     );
   }
 };
-const mapStateToProps = ({ screenConfiguration }) => {
-  const { preparedFinalObject = {} } = screenConfiguration;
-  const {
-    aboutUsContent = {
-      name: "Murali. M",
-      role: "Fullstack Developer",
-      skills: "JS, React JS, Node JS and DevOPS",
-      qualification: "B.E. in ISE,\nDiploma in CSE",
-      from: "Shimoga, Karnataka"
-    }
-  } = preparedFinalObject;
 
-  return {
-    aboutUsContent
-  };
-};
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(AboutUs));
+export default AboutUs;
