be65b701625fb72a70a67523b03f75d33555abfb
muralimgwl <murali.m@goodworklabs.com>
2020-04-19 20:46:56 +0530

    removed unused code

--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/FAQ/questions/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/FAQ/questions/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import { Card, CardContent, Grid, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
