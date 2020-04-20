5a89e9f62d1e048de95edfd63cd1fb1a461ce88c
muralimgwl <murali.m@goodworklabs.com>
2020-04-20 23:43:22 +0530

    removed grid

--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/AboutUs/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/AboutUs/index.js
@@ -1,34 +1,31 @@
 import React from "react";
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid } from "@material-ui/core";
 import Swiper from "../../../../../ui-containers/SwiperComponent";
 import { mapDispatchToProps } from "../../../../../ui-utils/commons";
 import { connect } from "react-redux";
 
 class AboutUs extends React.Component {
-
   render() {
-    const { setAppData, ourTeam, t} = this.props;
+    const { setAppData, ourTeam, t } = this.props;
 
     return (
       <div>
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Typography variant="h6" color="primary">
-          { t('About US') }
-      </Typography>
-        </Grid>
-      <Grid container style = {{position: "relative"}}>
-        <Grid item md = {12} sm = {12} xs = {12}>
-          <Swiper ourTeam={ourTeam} setAppData = {setAppData}/>
+        <Typography variant="h6" color="primary" align="center">
+          {t("About US")}
+        </Typography>
+        <Grid container style={{ position: "relative" }}>
+          <Grid item md={12} sm={12} xs={12}>
+            <Swiper ourTeam={ourTeam} setAppData={setAppData} />
           </Grid>
-      </Grid>
+        </Grid>
       </div>
     );
   }
-};
+}
 const mapStateToProps = ({ screenConfiguration }) => {
   const { preparedFinalObject = {} } = screenConfiguration;
-  const {appConfig = {}} = preparedFinalObject;
-  const {ourTeam = []} = appConfig;
+  const { appConfig = {} } = preparedFinalObject;
+  const { ourTeam = [] } = appConfig;
 
   return {
     ourTeam
