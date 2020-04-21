20a57a9527c1314429ba76152a666d34efaa81ba
muralimgwl <murali.m@goodworklabs.com>
2020-04-22 00:53:17 +0530

    implemented order by feature and code clean up

--- packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/Dashboard/index.js
+++ packages/mihy-ui-covid19-frontend/src/ui-pages/UserHome/components/Content/Dashboard/index.js
@@ -4,77 +4,151 @@ import { connect } from "react-redux";
 import { mapDispatchToProps } from "../../../../../ui-utils/commons";
 import { httpRequest } from "../../../../../ui-utils/api";
 import { withTranslation } from "react-i18next";
+import { Paper, Switch, Grid, Typography } from "@material-ui/core";
 // import YourArea from "./components/YourArea";
 import CountryStatus from "./components/CountryStatus";
 import TopList from "./components/TopList";
-import RemainingDays from "./components/RemainingDays"
+import RemainingDays from "./components/RemainingDays";
 import orderBy from "lodash/orderBy";
 
 class Dashboard extends React.Component {
   componentDidMount = async () => {
-    this.feathIndiaData()
-  }
+    this.feathIndiaData();
+  };
   feathIndiaData = async () => {
     let { setAppData, dashboard } = this.props;
+    const { checked } = dashboard;
     const dataResponse = await httpRequest({ endPoint: "data.json" });
-    const stateDistrictWiseResponse = await httpRequest({ endPoint: "state_district_wise.json" });
+    const stateDistrictWiseResponse = await httpRequest({
+      endPoint: "state_district_wise.json"
+    });
     dashboard = {
       ...dashboard,
       topList: dataResponse.statewise,
-      stateDistrictMapping: stateDistrictWiseResponse
-    }
-    setAppData("dashboard", dashboard)
-  }
+      stateDistrictMapping: stateDistrictWiseResponse,
+      checked: !checked
+    };
+    setAppData("dashboard", dashboard);
+  };
   feathWorldData = async () => {
     let { setAppData, dashboard } = this.props;
-    const dataResponse = await httpRequest({ endPoint: "https://corona.lmao.ninja/v2/all" });
-    const dataResponse1 = await httpRequest({ endPoint: "https://corona.lmao.ninja/v2/countries" });
+    const { checked } = dashboard;
+    const dataResponse = await httpRequest({
+      endPoint: "https://corona.lmao.ninja/v2/all"
+    });
+    let countriesResponse = await httpRequest({
+      endPoint: "https://corona.lmao.ninja/v2/countries"
+    });
+    if (countriesResponse) {
+      countriesResponse=orderBy(countriesResponse, ["cases"], ["desc"]);
+    }
     dashboard = {
       ...dashboard,
-      topList: dataResponse,
-      countriesMapping:dataResponse1
-    }
-    setAppData("dashboard", dashboard)
-  }
+      topList: dataResponse||{},
+      countriesMapping: countriesResponse||[],
+      checked: !checked
+    };
+    setAppData("dashboard", dashboard);
+  };
   handleClose = () => {
     this.props.setAppData("dashboard.dialogOpen", false);
   };
-  handleOpen = (selectedState) => {
+  handleOpen = selectedState => {
     let { setAppData, dashboard, history } = this.props;
     const { stateDistrictMapping = {} } = dashboard;
     let topDistrictList = stateDistrictMapping[selectedState] || {};
-    topDistrictList.districtData = topDistrictList.districtData || {}
-    topDistrictList = Object.keys(topDistrictList.districtData).map((key) => {
+    topDistrictList.districtData = topDistrictList.districtData || {};
+    topDistrictList = Object.keys(topDistrictList.districtData).map(key => {
       return {
         code: key,
         confirmed: topDistrictList.districtData[key].confirmed,
         delta: topDistrictList.districtData[key].delta.confirmed
-      }
-    })
+      };
+    });
     topDistrictList = orderBy(topDistrictList, ["confirmed"], ["desc"]);
     dashboard = {
       ...dashboard,
       selectedState,
       topDistrictList,
       dialogOpen: true
-    }
+    };
     setAppData("dashboard", dashboard);
     history.push("/user-home/districts-list");
   };
   handleStateSearch = (searchText = "") => {
     this.props.setAppData("dashboard.stateSearchText", searchText);
-  }
+  };
   render() {
-    const { dashboard, t} = this.props;
-    const { topList = [], stateSearchText } = dashboard;
-    const { handleOpen, handleStateSearch, feathIndiaData, feathWorldData } = this;
+    const { dashboard, t } = this.props;
+    const { topList = [], stateSearchText, checked = true } = dashboard;
+    const {
+      handleOpen,
+      handleStateSearch,
+      feathIndiaData,
+      feathWorldData
+    } = this;
     return (
       <div>
-        <CountryStatus t={t} feathIndiaData={feathIndiaData} feathWorldData={feathWorldData} countryStatus={topList.updated ? topList : topList.length > 0 ? topList[0] : {}} />
+        <CountryStatus
+          t={t}
+          // feathIndiaData={feathIndiaData}
+          // feathWorldData={feathWorldData}
+          countryStatus={
+            topList.updated ? topList : topList.length > 0 ? topList[0] : {}
+          }
+        />
         {topList.updated ? "" : <RemainingDays t={t} />}
         {/* <YourArea t={t} handleOpen={handleOpen} /> */}
-        {topList.updated ? <TopList t={t} handleOpen={handleOpen} topList={dashboard.countriesMapping} handleStateSearch={handleStateSearch} stateSearchText={stateSearchText} /> :
-        <TopList t={t} handleOpen={handleOpen} topList={topList} handleStateSearch={handleStateSearch} stateSearchText={stateSearchText} />}
+        {topList.updated ? (
+          <TopList
+            t={t}
+            handleOpen={handleOpen}
+            topList={dashboard.countriesMapping}
+            handleStateSearch={handleStateSearch}
+            stateSearchText={stateSearchText}
+          />
+        ) : (
+          <TopList
+            t={t}
+            handleOpen={handleOpen}
+            topList={topList}
+            handleStateSearch={handleStateSearch}
+            stateSearchText={stateSearchText}
+          />
+        )}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          <Paper
+            style={{
+              position: "fixed",
+              bottom: "62px",
+              padding: "0 8px",
+              borderRadius: "8%"
+            }}
+          >
+            <Typography component="div">
+              <Grid component="label" container alignItems="center" spacing={1}>
+                <Grid item>World</Grid>
+                <Grid item>
+                  <Switch
+                    checked={checked}
+                    onChange={(e) => {
+                      !checked ? feathIndiaData() : feathWorldData();
+                    }}
+                    name="ViewSwitch"
+                  />
+                </Grid>
+                <Grid item>India</Grid>
+              </Grid>
+            </Typography>
+          </Paper>
+        </div>
       </div>
     );
   }
@@ -82,13 +156,11 @@ class Dashboard extends React.Component {
 
 const mapStateToProps = ({ screenConfiguration }) => {
   const { preparedFinalObject = {} } = screenConfiguration;
-  const { dashboard = {}} = preparedFinalObject;
-  return { dashboard};
+  const { dashboard = {} } = preparedFinalObject;
+  return { dashboard };
 };
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(withTranslation()(Dashboard));
-
-
